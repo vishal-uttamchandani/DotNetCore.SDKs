503ef2ee2c8d37bc5df583ec0ac519d2bbec1a08
vishal <vishal@gmail.com>
2017-06-16 11:44:45 -0700

    core sdk 2.0.0-preview1-005977 webapi template

---
+++ coresdk.2.0.0-preview1-005977.webapi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace coresdk._2._0._0_preview1_005977.webapi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
